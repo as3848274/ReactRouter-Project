ceb5aa7829cf00fde38b4ae7914feb370548304b
Ayush Sharma <as3848274@gmail..com>
2025-11-28 18:59:36 +0530

    Initial commit - ReactRouter project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Header from './components/header/header'
+import Home from './components/Home/Home'
+import Footer from './components/footer/Footer'
+
+
+
+
+function App() {
+
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
